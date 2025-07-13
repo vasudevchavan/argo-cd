da096d8444da9275f3fbccbc13ad28cac0a4f299
vasudevchavan <vasudevdchavan@gmail.com>
2025-07-13 23:47:50 +0530

    new changes

--- terraform/0-provider.tf
+++ terraform/0-provider.tf
@@ -1,6 +1,23 @@
-provider "helm" {
-    kubernetes = {
-        config_path = "~/.kube/config"
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
 }
 
